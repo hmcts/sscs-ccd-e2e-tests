bb4ceaf27d5293d4845593b5a8d4f1adaa87fab9
PhaniP <phani.perla@hmcts.net>
2021-05-19 11:04:24 +0100

    update

--- e2e/features.crossbrowser.conf.js
+++ e2e/features.crossbrowser.conf.js
@@ -28,11 +28,11 @@ const config = {
   sauceRegion: 'eu',
   port: 80,
   sauceConnect: true,
-  webDriverProxy: 'http://proxyout.reform.hmcts.net:8080',
   sauceProxy: 'http://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
+  webDriverProxy: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
 
   specs: ['./features/*.feature'],
   baseUrl: serviceConfig.CcdWebUrl,
