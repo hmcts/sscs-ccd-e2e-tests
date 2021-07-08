8270881d52af32c2ba629be45919fc11523c32f9
gokul-sol <gokul.sridharan@HMCTS.NET>
2021-07-08 14:16:58 +0100

    conditions to not run test steps for cross-browser

--- e2e/features/step_definitions/authentication.steps.ts
+++ e2e/features/step_definitions/authentication.steps.ts
@@ -5,10 +5,13 @@ import { browser } from 'protractor';
 
 const anyCcdPage = new AnyCcdPage();
 const authenticationFlow = new AuthenticationFlow();
+const serviceConfig = require('../../service.conf');
 
 Given(/^I am signed in as a Case Officer$/, async function () {
     await authenticationFlow.signInAsCaseOfficer();
-    await anyCcdPage.waitUntilLoaded();
+    if(!serviceConfig.TestsForCrossBrowser){
+        await anyCcdPage.waitUntilLoaded();
+    }
 });
 
 Given(/^I am signed in as a DWPResponse Writer$/, async function () {
