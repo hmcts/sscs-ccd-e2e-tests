dbf4015e1890117cc9d1acca8202e1a37979c59e
Divya Arun <divya.arun@enginegroup.com>
2021-09-15 11:17:15 +0100

    SSCS-9379:Hearing recording feature

---
+++ e2e/features/step_definitions/hearing-recording.steps.ts
@@ -0,0 +1,60 @@
+import { browser } from 'protractor';
+import { AnyCcdPage } from '../../pages/any-ccd.page';
+// @ts-ignore
+import { HearingRecordingPage } from '../../pages/hearing-recording.page';
+import { CaseDetailsPage } from '../../pages/case-details.page';
+import { Then, When } from 'cucumber';
+import { expect } from 'chai';
+import { delay } from 'rxjs/operators';
+
+const anyCcdPage = new AnyCcdPage();
+const hearingRecordingPage = new HearingRecordingPage();
+const caseDetailsPage = new CaseDetailsPage();
+
+When(/^I upload a hearing recording$/, async function () {
+  await hearingRecordingPage.uploadHearingRecording();
+});
+
+When(/^I select a hearing$/, async function () {
+  expect(await anyCcdPage.pageHeadingContains('Upload hearing recording')).to.equal(true);
+  await hearingRecordingPage.selectHearing();
+  await browser.sleep(500);
+  await anyCcdPage.click('Continue');
+});
+
+Then(/^the hearing recording should (be|not be) in "(.+)" tab$/, async function (seeOrNotSee, tabName) {
+  const isDisplayed = (seeOrNotSee === 'be');
+  await browser.sleep(500);
+  await anyCcdPage.reloadPage();
+  await anyCcdPage.clickTab(tabName);
+  expect(await anyCcdPage.contentContains('recordings 1')).to.equal(isDisplayed);
+  expect(await anyCcdPage.contentContains('Recordings')).to.equal(isDisplayed);
+  expect(await anyCcdPage.contentContains('Final Hearing')).to.equal(isDisplayed);
+  expect(await anyCcdPage.contentContains('12345')).to.equal(isDisplayed);
+  expect(await anyCcdPage.contentContains('Fox Court')).to.equal(isDisplayed);
+  await browser.sleep(5000);
+});
+
+Then(/^the "(.+)" should be successfully listed in "(.+)" tab$/, async function (action, tabName) {
+  await delay(10000);
+  await caseDetailsPage.reloadPage();
+  await anyCcdPage.clickTab(tabName);
+  expect(await caseDetailsPage.eventsPresentInHistory(action)).to.equal(true);
+  await browser.sleep(500);
+});
+
+When(/^I request for Hearing recording$/, async function () {
+  expect(await anyCcdPage.pageHeadingContains('Request hearing recording')).to.equal(true);
+  await hearingRecordingPage.requestDwpHearingRecording();
+  await anyCcdPage.click('Continue');
+  await anyCcdPage.click('Submit');
+  await browser.sleep(500);
+});
+
+When(/^I grant request for Hearing recording$/, async function () {
+  expect(await anyCcdPage.pageHeadingContains('Please review the hearing recordings')).to.equal(true);
+  await hearingRecordingPage.grantRequestDwpHearingRecording();
+  await anyCcdPage.click('Continue');
+  await anyCcdPage.click('Submit');
+  await browser.sleep(500);
+});
---
+++ e2e/pages/hearing-recording.page.ts
@@ -0,0 +1,47 @@
+import { browser, by, element } from 'protractor';
+import { AnyPage } from './any.page';
+import { AnyCcdPage } from './any-ccd.page';
+import * as path from 'path';
+
+const anyCcdPage = new AnyCcdPage();
+
+export class HearingRecordingPage extends AnyPage {
+
+    async addHearingRecording() {
+        await element(by.id('hearingRecording_recordings'))
+            .element(by.xpath('//*[@id="hearingRecording_recordings"]/div/button[1]')).click();
+        await browser.sleep(500);
+        await browser.waitForAngular();
+        let remote = require('selenium-webdriver/remote');
+        browser.setFileDetector(new remote.FileDetector());
+        await this.uploadFile('hearingRecording_recordings_value', 'test_av.mp3');
+        await browser.sleep(5000);
+    }
+
+    async uploadFile(inputElement: string, fileName: string) {
+        let fileToUpload = '../dwpResponse/' + fileName,
+            absolutePath = path.resolve(__dirname, fileToUpload);
+        await element(by.id(inputElement)).sendKeys(absolutePath);
+    }
+
+    async uploadHearingRecording() {
+        await anyCcdPage.clickElementById('hearingRecording_hearingType-final');
+        await this.addHearingRecording();
+        await anyCcdPage.click('Continue');
+        await browser.sleep(500);
+        await anyCcdPage.click('Submit');
+    }
+
+    async selectHearing() {
+        await element(by.id('selectHearingDetails')).element(by.xpath('//*[@id="selectHearingDetails"]/option[2]')).click();
+    }
+
+    async requestDwpHearingRecording() {
+        await element(by.id('requestableHearingDetails')).element(by.xpath('//*[@id="requestableHearingDetails"]/option[2]')).click();
+    }
+
+    async grantRequestDwpHearingRecording() {
+        await element(by.id('processHearingRecordingRequests_0_dwp'))
+            .element(by.xpath('//*[@id="processHearingRecordingRequests_0_dwp"]/option[2]')).click();
+    }
+}
