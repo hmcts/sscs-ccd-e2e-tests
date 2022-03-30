27881a9cd5c3c940202c2fbacbefa4a4135c59e2
gokul-sol <gokul.sridharan@HMCTS.NET>
2022-03-30 17:04:09 +0100

    fix lint issues

--- e2e/features/step_definitions/case-access-maangement.steps.ts
+++ e2e/features/step_definitions/case-access-maangement.steps.ts
@@ -1,7 +1,7 @@
 
 import { AnyCcdPage } from '../../pages/any-ccd.page';
 import { Given } from 'cucumber';
-import { assert} from 'chai';
+import { assert } from 'chai';
 
 const anyCcdPage = new AnyCcdPage();
 
@@ -9,12 +9,11 @@ function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-
-    Given('{string} tab should contain {string} value for case management {string} field',  async function (tabName, fieldValue, fieldName) {
+Given('{string} tab should contain {string} value for case management {string} field',  async function (tabName, fieldValue, fieldName) {
     await delay(5000);
     await anyCcdPage.clickTab(tabName);
     await delay(10000);
     await anyCcdPage.getFieldValue(fieldName).then(function(actText) {
         assert.equal(actText, fieldValue);
-    });    
+    });
 });
