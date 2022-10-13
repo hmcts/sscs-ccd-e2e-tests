d491c4dc1a32cbc5cdd0a0b75380e1109a987fd4
venkataakepati <venkata.akepati@hmcts.net>
2022-10-13 18:46:14 +0100

    amend hearing

--- e2e/pages/update-listing-requirements.page.ts
+++ e2e/pages/update-listing-requirements.page.ts
@@ -7,25 +7,25 @@ const anyCcdPage = new AnyCcdPage();
 
 export class UpdateListingRequirementsPage extends AnyPage {
 
- async updateHearingChannel(video: string) {
-    expect(await anyCcdPage.pageHeadingContains('Update Listing Requirements')).to.equal(true);
+ async updateHearingChannel(hearingType) {
     await browser.sleep(500);
-    await anyCcdPage.chooseOptionByElementId('#overrideFields_appellantHearingChannel', video);
+     expect(await anyCcdPage.pageHeadingContains('Update Listing Requirements')).to.equal(true);
+     await browser.sleep(500);
+    await anyCcdPage.chooseOptionContainingText('#overrideFields_appellantHearingChannel', video);
+  }
+  async updatePOOfficerAttending(AttendingOfficer) {
+    await browser.sleep(500);
+     await anyCcdPage.chooseOptionContainingText('#overrideFields_poToAttend_Yes', yes);
+     await browser.sleep(500);
+     await anyCcdPage.click('Continue');
+  }
+  async amendReasonForUpdate() {
+    expect(await anyCcdPage.pageHeadingContains('Amend Reason')).to.equal(true);
+     await browser.sleep(500);
+     await anyCcdPage.click('Judge requested change');
+     await browser.sleep(500);
+     await anyCcdPage.click('Continue');
+     await browser.sleep(500);
+     await anyCcdPage.click('Submit');
+  }
  }
-
- async updatePOOfficerAttending(yes: string) {
-    await anyCcdPage.chooseOptionByElementId('#overrideFields_poToAttend_Yes', yes);
-    await browser.sleep(500);
-    await anyCcdPage.click('Continue');
- }
-
- async amendReasonForUpdate() {
-   expect(await anyCcdPage.pageHeadingContains('Amend Reason')).to.equal(true);
-    await browser.sleep(500);
-    await anyCcdPage.click('Judge requested change');
-    await browser.sleep(500);
-    await anyCcdPage.click('Continue');
-    await browser.sleep(500);
-    await anyCcdPage.click('Submit');
- }
-}
