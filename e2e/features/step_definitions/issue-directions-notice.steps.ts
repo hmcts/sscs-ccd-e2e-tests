75fb7f88644a61196fe216a7f0676da6259ea414
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-12-25 12:21:33 +0000

    Wait for preview document

--- e2e/features/step_definitions/issue-directions-notice.steps.ts
+++ e2e/features/step_definitions/issue-directions-notice.steps.ts
@@ -1,17 +1,18 @@
 import { Then, When } from 'cucumber';
 import { AnyCcdPage } from '../../pages/any-ccd.page';
 import { expect } from 'chai';
-import { browser } from 'protractor';
+import { by } from 'protractor';
+
 const anyCcdPage = new AnyCcdPage();
 
 When('I allow the appeal to proceed', async function () {
   await anyCcdPage.chooseOptionContainingText('directionTypeDl', 'Appeal to Proceed');
-  await browser.sleep(3000);
   await anyCcdPage.scrollPage('//*[@id="generateNotice_Yes"]');
   await anyCcdPage.fillValues('bodyContent', 'This is a test body content');
   await anyCcdPage.fillValues('signedBy', 'This is a test signed content');
   await anyCcdPage.fillValues('signedRole', 'This is a test signed role content');
   await anyCcdPage.clickContinue();
+  await anyCcdPage.waitForElement(by.xpath('//span[contains(text(),"Preview Document")]'));
   await anyCcdPage.clickContinue();
   await anyCcdPage.clickSubmit();
 });
