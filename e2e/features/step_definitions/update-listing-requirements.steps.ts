6ca838ab3ea72f83ba848373bb36fcc003f2d02e
ReachmeonGit <ReachmeonGit@github.com>
2023-09-12 12:28:47 +0100

    update listing requirements

--- e2e/features/step_definitions/update-listing-requirements.steps.ts
+++ e2e/features/step_definitions/update-listing-requirements.steps.ts
@@ -3,7 +3,7 @@ import { UpdateListingRequirementsPage } from '../../pages/update-listing-requir
 
 const updateListingRequirementsPage = new UpdateListingRequirementsPage();
 
-Given('I choose (.+)" option from appellant hearing channel', async function (video) {
+Given('I choose "(.+)" option from appellant hearing channel', async function (video) {
      await updateListingRequirementsPage.updateHearingChannel(video);
 });
 
