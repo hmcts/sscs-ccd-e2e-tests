45ff592fb01e50bd19eabf1c077ff676a100b99e
ReachmeonGit <ReachmeonGit@github.com>
2023-09-12 18:23:12 +0100

    fixed lint issues

--- e2e/features/step_definitions/request-hearing-details.steps.ts
+++ e2e/features/step_definitions/request-hearing-details.steps.ts
@@ -7,71 +7,69 @@ const anyCcdPage = new AnyCcdPage();
 const hearingDetailsPage = new HearingDetailsPage();
 
 Given('I click on Request Hearing link', async function () {
-     await hearingDetailsPage.requestManualHearing();
-     await hearingDetailsPage.verifyHearingStatusSummary();
+  await hearingDetailsPage.requestManualHearing();
+  await hearingDetailsPage.verifyHearingStatusSummary();
 });
 
 Then('I should see a hearing request generated for the appeal', async function () {
-     await hearingDetailsPage.requestAutoHearing();
-     await hearingDetailsPage.verifyHearingStatusSummary();
+  await hearingDetailsPage.requestAutoHearing();
+  await hearingDetailsPage.verifyHearingStatusSummary();
 });
 
 Then('I click on hearing details', async function () {
-     await hearingDetailsPage.viewHearingDetails();
+  await hearingDetailsPage.viewHearingDetails();
 });
 
-Then('the venue of the hearing should be in "(.+)"$', async function (venueName: string) {
-     await hearingDetailsPage.verifyHearingVenue(venueName);
+Then('the venue of the hearing should be in {string}$', async function (venueName: string) {
+  await hearingDetailsPage.verifyHearingVenue(venueName);
 });
 
-Then('the duration of the hearing should be "(.+)"$', async function (hearingDuration: string) {
-     await hearingDetailsPage.verifyHearingDuration(hearingDuration);
+Then('the duration of the hearing should be {string}$', async function (hearingDuration: string) {
+  await hearingDetailsPage.verifyHearingDuration(hearingDuration);
 });
 
-Then('the earliest hearing date should be from "(.+)" days of hearing requested$', async function (noOfDays: string) {
+Then('the earliest hearing date should be from {string} days of hearing requested$', async function (noOfDays: string) {
+  let date = new Date();
+  let numberOfDaysToAdd = Number(noOfDays);
+  let result = date.setDate(date.getDate() + numberOfDaysToAdd);
 
-     let date = new Date();
-     let numberOfDaysToAdd = Number(noOfDays);
-     let result = date.setDate(date.getDate() + numberOfDaysToAdd);
-
-     const additionalDate = new Date(result);
-     const formattedDate = additionalDate.toLocaleDateString('en-GB', {
-     day: 'numeric', month: 'long', year: 'numeric'
+  const additionalDate = new Date(result);
+  const formattedDate = additionalDate.toLocaleDateString('en-GB', {
+  day: 'numeric', month: 'long', year: 'numeric'
      }).replace(/ /g, ' ');
 
-     await hearingDetailsPage.verifyHearingDuration(formattedDate);
+  await hearingDetailsPage.verifyHearingDuration(formattedDate);
 });
 
-Then('I update the length of hearing to "(.+)" hours$', async function (hearingDuration: string) {
-     await hearingDetailsPage.updateHearingDetails(hearingDuration);
+Then('I update the length of hearing to {string} hours', async function (hearingDuration: string) {
+  await hearingDetailsPage.updateHearingDetails(hearingDuration);
 });
 
-Then('the hearing status should be updated to "(.+)"$', async function (hearingStatus: string) {
-     await hearingDetailsPage.verifyHearingStatus(hearingStatus);
+Then('the hearing status should be updated to {string}$', async function (hearingStatus: string) {
+  await hearingDetailsPage.verifyHearingStatus(hearingStatus);
 });
 
 Then('I click on Hearings tab$/', async function () {
-       await hearingDetailsPage.verifyHearingStatusSummary();
+  await hearingDetailsPage.verifyHearingStatusSummary();
 });
 
-When('I click on "(.+)" hearing link and select "(.+)" as cancellation reason$', async function (cncl: string, reason: string) {
-        await anyCcdPage.click(cncl);
-        await browser.sleep(3000);
-
-        await anyCcdPage.clickElementById(reason);
-        await anyCcdPage.click('Continue');
-        await browser.sleep(5000);
+When('I click on "(.+)" hearing link and select {string} as cancellation reason', async function (cncl: string, reason: string) {
+  await anyCcdPage.clickButton(cncl);
+  await browser.sleep(3000);
+  await anyCcdPage.clickElementById(reason);
+  await anyCcdPage.clickButton('Continue');
+  await browser.sleep(5000);
 });
 
-Then(/'I should see "(.+)" hearing status in summary page$', async function (hearingStats: string) {
-     await hearingDetailsPage.verifyCancelHearingStatus(hearingStats);
+Then('I should see {string} hearing status in summary page', async function (hearingStats: string) {
+  await hearingDetailsPage.verifyCancelHearingStatus(hearingStats);
 });
 
-When('submit the event$/', async function () {
-     await anyCcdPage.click('Submit');
+When('submit the event', async function () {
+  await anyCcdPage.clickButton('Submit');
 });
 
-Then('the hearing status should be "(.+)"$', async function (hearingStats: string) {
-     await hearingDetailsPage.requestAutoHearing();
-     await hearingDetailsPage.verifyCancelHearingStatus(hearingStats);
+Then('the hearing status should be {string}$', async function (hearingStats: string) {
+  await hearingDetailsPage.requestAutoHearing();
+  await hearingDetailsPage.verifyCancelHearingStatus(hearingStats);
 });
